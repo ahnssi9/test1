fa6748ecc388d745396c17a0a0fd5b872f525a1f
ahnssi9 <ahnssi9@naver.com>
2024-11-15 16:17:24 +0900

    chap10 드디어 홀로 올린단다

---
+++ src/ThrowTest.java
@@ -0,0 +1,9 @@
+
+
+public class ThrowTest {
+		public void goRun()  throws Exception{
+			
+			int a = 3/0;
+			
+		}
+}
---
+++ src/chap10throws/main.java
@@ -0,0 +1,10 @@
+package chap10throws;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
